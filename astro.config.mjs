03ebd747cfb4584fd83d071ba05434626cd4f2fe
Pavel Reznikov <pashka.reznikov@gmail.com>
2024-05-25 15:30:22 -0700

    add keystatic

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,12 @@
 import { defineConfig } from 'astro/config';
-
 import cloudflare from "@astrojs/cloudflare";
+import keystatic from '@keystatic/astro';
+import react from "@astrojs/react";
+import markdoc from "@astrojs/markdoc";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
-  adapter: cloudflare()
+  adapter: cloudflare(),
+  integrations: [react(), markdoc(), keystatic()]
 });
